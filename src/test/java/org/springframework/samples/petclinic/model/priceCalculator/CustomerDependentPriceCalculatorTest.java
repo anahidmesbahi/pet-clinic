0f2e484ab276706a1dd7d7ec50c31e74474da805
Anahid Mesbahi <anahidmesbahi@gmail.com>
2021-11-15 20:07:53 +0330

    simplecalc added

--- src/test/java/org/springframework/samples/petclinic/model/priceCalculator/CustomerDependentPriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/model/priceCalculator/CustomerDependentPriceCalculatorTest.java
@@ -1,6 +1,10 @@
 package org.springframework.samples.petclinic.model.priceCalculator;
 
 import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Assume;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.model.UserType;
@@ -19,9 +23,16 @@ public class CustomerDependentPriceCalculatorTest {
 		Pet P3 = new Pet();
 
 		PetType T1 = new PetType();
+		PetType T2 = new PetType();
+		T1 = mock(T1.getClass());
+		T2 = mock(T2.getClass());
+
+		when(T1.getRare()).thenReturn(true);
+		when(T2.getRare()).thenReturn(false);
 
 		P1.setType(T1);
 		P2.setType(T1);
+		P3.setType(T2);
 
 		P1.setBirthDate(new Date());
 		P2.setBirthDate(new Date(974289794));
@@ -43,6 +54,69 @@ public class CustomerDependentPriceCalculatorTest {
 		CustomerDependentPriceCalculator C1 = new CustomerDependentPriceCalculator();
 		double result = C1.calcPrice(pets, baseCharge, basePricePerPet, userType);
 
+		Assert.assertEquals(result, result);
 
+	}
+
+	@Test
+	public void calcPriceTestNewUser() {
+		List<Pet> pets = new ArrayList<>();
+		Pet P1 = new Pet();
+		Pet P2 = new Pet();
+		Pet P3 = new Pet();
+
+		PetType T1 = new PetType();
+
+		P1.setType(T1);
+		P2.setType(T1);
+
+		P1.setBirthDate(new Date());
+		P2.setBirthDate(new Date(974289794));
+		P3.setBirthDate(new Date());
+
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P1);
+		pets.add(P2);
+		pets.add(P3);
+		double baseCharge = 15.5;
+		double basePricePerPet  = 7.35;
+		UserType userType = UserType.NEW;
+
+		CustomerDependentPriceCalculator C1 = new CustomerDependentPriceCalculator();
+		double result = C1.calcPrice(pets, baseCharge, basePricePerPet, userType);
+
+		Assert.assertEquals(result, result);
+	}
+
+	@Test
+	public void calcPriceTestGoldUser() {
+		List<Pet> pets = new ArrayList<>();
+		Pet P1 = new Pet();
+
+		PetType T1 = new PetType();
+
+		P1.setType(T1);
+
+		P1.setBirthDate(new Date());
+
+		pets.add(P1);
+		double baseCharge = 15.5;
+		double basePricePerPet  = 7.35;
+		UserType userType = UserType.GOLD;
+
+		CustomerDependentPriceCalculator C1 = new CustomerDependentPriceCalculator();
+		double result = C1.calcPrice(pets, baseCharge, basePricePerPet, userType);
+
+		Assert.assertEquals(result, result);
 	}
 }
--- src/test/java/org/springframework/samples/petclinic/model/priceCalculator/SimplePriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/model/priceCalculator/SimplePriceCalculatorTest.java
@@ -1,4 +1,56 @@
 package org.springframework.samples.petclinic.model.priceCalculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.UserType;
+import org.springframework.samples.petclinic.model.priceCalculators.SimplePriceCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class SimplePriceCalculatorTest {
+	@Test
+	public void calcPriceTest(){
+		List<Pet> pets = new ArrayList<>();
+		Pet P1 = new Pet();
+		Pet P2 = new Pet();
+		Pet P3 = new Pet();
+
+		PetType T1 = new PetType();
+		PetType T2 = new PetType();
+
+		T1 = mock(T1.getClass());
+		T2 = mock(T2.getClass());
+
+		when(T1.getRare()).thenReturn(true);
+		when(T2.getRare()).thenReturn(false);
+
+		P1.setType(T1);
+		P2.setType(T1);
+		P3.setType(T2);
+
+		pets.add(P1);
+		pets.add(P2);
+		pets.add(P2);
+		pets.add(P2);
+		pets.add(P2);
+		pets.add(P2);
+		pets.add(P2);
+		pets.add(P2);
+		pets.add(P3);
+
+		double baseCharge = 15.5;
+		double basePricePerPet  = 7.35;
+
+		UserType userType = UserType.GOLD;
+
+		SimplePriceCalculator S1 = new SimplePriceCalculator();
+		double result = S1.calcPrice(pets, baseCharge, basePricePerPet, userType);
+		Assert.assertEquals(result, result);
+	}
 }
